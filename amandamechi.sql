b0bda4cb43c82e35e2a2d4721d5343dca8843e8c
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-11-19 09:40:58 -0300

    Criação das tabelas

---
+++ amandamechi.sql
@@ -0,0 +1,23 @@
+CREATE TABLE dicas (
+    id_dica INT /*PK?*/
+    titulo VARCHAR(250),
+    disciplina VARCHAR(50),
+    conteudo: TEXT
+)
+
+CREATE TABLE carreiras(
+    id_carreira INT (PK),
+    nome_carreira VARCHAR(250),
+    disciplina VARCHAR /*BOOLEAN??*/
+    /*imagem VARCHAR (250),*/
+    requisitos TEXT,
+    descricao TEXT,
+    area_atuaca TEXT,
+    disciplina VARCHAR(50),
+    habilidades_necessarias TEXT
+)
+
+
+
+
+
