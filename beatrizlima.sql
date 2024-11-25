dfb6387c4a63471a205c536cfb1a7ccbcb5241c4
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-25 16:10:57 -0300

    feat: sobre_nos inserts

--- beatrizlima.sql
+++ beatrizlima.sql
@@ -3,7 +3,7 @@ CREATE DATABASE portaldenoticias;
 CREATE TABLE autores (
     id_autor SERIAL PRIMARY KEY,
     nome_autor VARCHAR(100) NOT NULL,
-)
+);
 
 CREATE TABLE noticias (
     id_noticia SERIAL PRIMARY KEY,
@@ -27,8 +27,15 @@ CREATE TABLE entrevistas (
 
 CREATE TABLE sobre_nos (
     id_integrante SERIAL PRIMARY KEY,
-    nove_inetegrante VARCHAR(100),
+    nome_integrante VARCHAR(100),
     foto_integrante VARCHAR(200),
     funcao_scrum VARCHAR(15),
     email_integrante VARCHAR(200)
 );
+
+INSERT INTO sobre_nos(nome_integrante, foto_integrante, funcao_scrum, email_integrante) VALUES
+('Amanda Gomes Mechi', '/Portal_Noticias/images/Amanda.site.jpeg', 'Product Owner', 'amanda.mechi@aluno.senai.br' ),
+('André Lucca Santos', '/Portal_Noticias/images/André.site.jpeg', 'Desenvolvedor', 'andre.r.santos45@aluno.senai.br'),
+('Beatriz Lima', '/Portal_Noticias/images/Beatriz.site.jpeg', 'Desenvolvedora', 'beatriz.lima14@aluno.senai.br'),
+('Enzo Alves Turcovic', '/Portal_Noticias/images/Enzo.site.jpeg', 'Scrum Master', 'enzo.turcovic@aluno.senai.br'),
+('Leonardo Pedro de Oliveira', '/Portal_Noticias/images/Leonardo.site.jpeg', 'Desenvolvedor', 'leonardo.p.oliveira12@aluno.senai.br')
