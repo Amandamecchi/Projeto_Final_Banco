578984c7fbd5eb257d813cb9a2f2300c9d53731d
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2024-11-26 17:23:06 -0300

    Create enzoturcovic.sql

---
+++ enzoturcovic.sql
@@ -0,0 +1,18 @@
+CREATE TABLE carreiras (
+id SERIAL PRIMARY KEY,
+area VARCHAR(50),
+titulo_principal VARCHAR(50),
+imagem_principal VARCHAR (300),
+descricao TEXT,
+pre_requisitos TEXT,
+areas_atuacao VARCHAR(50),
+formacao_academia VARCHAR(50),
+mercado_trabalho TEXT
+);
+
+INSERT INTO carreiras (area, titulo_principal, imagem_principal, descricao, pre_requisitos, areas_atuacao, formacao_academia, mercado_trabalho) VALUES
+('arte', 'Artes Visuais', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com) EXEMPLO
+
+('Educação Física', 'Conhecimentos Gerais') EXEMPLO
+
+
