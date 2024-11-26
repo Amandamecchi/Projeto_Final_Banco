71c103340befe806152062c2ecf8c2cb4b18a8a9
Amanda Gomes Mechi <amandamechi327@gmail.com>
2024-11-26 17:19:48 -0300

    Correções

--- amandamechi.sql
+++ amandamechi.sql
@@ -1,40 +1,18 @@
 CREATE TABLE dicas (
-    id_dica INT /*PK?*/
-    titulo VARCHAR(250),
-    disciplina VARCHAR(50),
-    conteudo: TEXT
-)
-
-CREATE TABLE carreiras(
-  id_carreira INT (PK),
-  nome_carreira VARCHAR(255),
-  disciplina VARCHAR(50),
-  /*imagem VARCHAR(255)*/,
-  requisitos TEXT,
-  descricao TEXT,
-  areas_atuacao TEXT
-)
+    id_dica SERIAL PRIMARY KEY,
+    titulo_principal VARCHAR(250),
+    area VARCHAR(50),
+    descricao TEXT,
+    url_imagem VARCHAR(1000)
+);
 
 /*INSERTS - DICAS*/
 
-INSERT INTO dicas (id_dica, nome_carreira, disciplina, /*img*/, requisitos, descricao, areas_atuacao) VALUES
-/*seria mais fácil criar ma tabela para cada materia, tá confundindo a minha cabeça*/
-(1, 'Teoria da Arte', 'Artes', /**/),
-(2, 'Historia da Arte', 'Artes', /**/),
-(3, 'Elementos da Arte', 'Artes', /**/),
-(4, 'A Importância do Aquecimento', 'Educação Física', /**/),
-(5, 'Fisiologia do Exercício', 'Educação Física', /**/),
-(6, 'Benefícios da Atividade Física', 'Educação Física', /**/),
+INSERT INTO dicas (id_dica, titulo_principal, area, descricao, url_imagem) VALUES
+(1, 'Teoria da Arte', 'Artes',  'Como dizia Aristóteles, "a arte imita a vida". A teoria da arte examina como e por que criamos, interpretamos e valorizamos o que chamamos de arte. Tem a ver com estética (beleza, feiura), semiótica (significados) e até política e sociologia (influências culturais e sociais)', 'https://static.escolakids.uol.com.br/2024/07/professora-dando-aula-de-alongamento-durante-a-disciplina-de-educacao-fisica-para-uma-turma-de-criancas-em-um-ginasio.jpg'),
+(2, 'Historia da Arte', 'Artes', 'A história da arte é a narração da evolução das expressões artísticas ao longo dos tempos. É um olhar profundo sobre como diferentes culturas e períodos históricos influenciaram as formas de arte, desde as primeiras pinturas rupestres até as criações contemporâneas','https://static.escolakids.uol.com.br/2024/07/professora-dando-aula-de-alongamento-durante-a-disciplina-de-educacao-fisica-para-uma-turma-de-criancas-em-um-ginasio.jpg'),
+(3, 'Elementos da Arte', 'Artes', 'Os elementos da arte são como um alfabeto no mundo artísitco, essenciais para a criação visual. Linhas e artes, forma, cor, valor, espaço, textura e volume, esses são os blocos de construção que os artistas usam para expressar suas ideias e criar sua composições.', 'https://static.escolakids.uol.com.br/2024/07/professora-dando-aula-de-alongamento-durante-a-disciplina-de-educacao-fisica-para-uma-turma-de-criancas-em-um-ginasio.jpg'),
+(4, 'A Importância do Aquecimento',  'Educação Física', 'O aquecimento prepara a musculatura, as articulações, o sistema nervoso e o sistema cardíaco para que eles respondam melhor aos estímulos das atividades físicas. Os músculos são os que executam os movimentos e, por essa razão, precisam de oxigênio suficiente. É durante o aquecimento que a pessoa antecipa o metabolismo aeróbico que vai estabilizar a produção de energia durante as atividades.', 'https://static.escolakids.uol.com.br/2024/07/professora-dando-aula-de-alongamento-durante-a-disciplina-de-educacao-fisica-para-uma-turma-de-criancas-em-um-ginasio.jpg'),
+(5, 'Fisiologia do Exercício', 'Educação Física', 'A Fisiologia do Exercício, área de conhecimento derivada da Fisiologia, é caracterizada pelo estudo dos efeitos agudos e crônicos do exercício físico sobre as estruturas e as funções dos sistemas do corpo humano.', 'https://static.escolakids.uol.com.br/2024/07/professora-dando-aula-de-alongamento-durante-a-disciplina-de-educacao-fisica-para-uma-turma-de-criancas-em-um-ginasio.jpg'),
+(6, 'Benefícios da Atividade Física', 'Educação Física', 'O aquecimento prepara a musculatura, as articulações, o sistema nervoso e o sistema cardíaco para que eles respondam melhor aos estímulos das atividades físicas.', 'https://static.escolakids.uol.com.br/2024/07/professora-dando-aula-de-alongamento-durante-a-disciplina-de-educacao-fisica-para-uma-turma-de-criancas-em-um-ginasio.jpg');
 
-/*INSERTS - CARREIRAS*/
-
-INSERT INTO carreiras (id_carreira, nome_carreira, disciplina, /*imagem*/, requisitos, descricao, areas_atuacao) VALUES
-(1, 'Ator', 'Artes', 'img?', 'Cursos Profissionalizantes,' 'A carreira de ator envolve a interpretação de personagens em diversos tipos de mídia, como cinema, teatro, televisão e até plataformas digitais.', 'Cinema, Teatro, Televisão, Dublagem de voz, Comerciais e Publicidade, Performances em Eventos'),
-(2, 'Músico', 'Artes', 'img', 'Edcação Musical, Teoria Musical, Habilidade Musical, Dedicação', 'Um músico é um artista multifacetado que se dedica à criação, interpretação e ensino de música.', 'Música ao Vivo, Ensino de Música, Composição e arranjo, Produção Musical, Orquestra e Bandas Sinfônicas'),
-(3, 'Dançarino', 'Artes', 'img',  'Experiência, Criatividade, certificações pedagógicas', 'O dançarino é o profissional que trabalha com dança. Ele é capaz de se expressar através do seu corpo, com movimentos artísticos que dão vida à música.', 'Ensino, Direção, Produção de Eventos, Expressão Corporal',)
-(4, 'Artista Visual', 'Artes', 'img',  'Graduação ou Cursos Técnicos, Habilidades Artísticas, Desenvolvimento de Portfólio, Estilo Pessoal', 'O profissional de Artes Visuais é responsável pelo desenvolvimento e divulgação de obras de arte, elaboração de projetos de design e comunicação visual, além da aplicação de estratégias para a conservação de bens culturais.', 'Ensino e Educação, Produções Artísticas, Design e Comunicação Visual,  Produção Cultural, Restauro e Conservação, Mídias Digitais e Multimídia, Mercado de Arte, Crítica, Avaliação e Análise de Arte, Cenografia e Figurino'),
-
-(5, 'Ensino', 'Educação Física', 'img', 'Interesse por Esportes e Saúde, Habilidades de Comunicação, Capacidade de liderança e motivação, Formação acadêmica, Empatia e Sensibilidade', 'O profissional de educação física atua em diversas frentes, contribuindo para a formação de indivíduos saudáveis e ativos.', 'Escolas, Academias, Clubes Esportivos, Saúde Pública, Reabilitação Física, Pesquisa e Ensino Superior' )
-(5, 'Ensino', 'Educação Física', 'img', '',)
-(5, 'Ensino', 'Educação Física', 'img', '',)
-(5, 'Ensino', 'Educação Física', 'img', '',)
